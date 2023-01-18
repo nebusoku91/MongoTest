956cd8726090d6f583d8c86b6c8ec4e5eccf92e1
Jonathan Strand <jontepwn@gmail.com>
2023-01-18 16:06:27 +0100

    final

--- MongoTest/Program.cs
+++ MongoTest/Program.cs
@@ -17,9 +17,6 @@ namespace MongoTest
 
         public void Start()
         {
-            var database = dbClient.GetDatabase("Product");
-            var gamesCollection = database.GetCollection<BsonDocument>("Games");
-
             while (true)
             {
                 Console.WriteLine("= Main Menu =\n" +
@@ -54,7 +51,7 @@ namespace MongoTest
                             Exit();
                             break;
                         default:
-                            Console.WriteLine("Fel");
+                            Console.WriteLine("Invalid input. Please try again.");
                             break;
                     }
                 }
@@ -70,59 +67,51 @@ namespace MongoTest
             Console.WriteLine("1. All games\n2. Action games\n3. Roleplaying Games\n4. Strategy games\n5. Return");
             Console.Write("> ");
 
-            try
-            {
-                if (Int32.TryParse(Console.ReadLine(), out input))
-                {
-                    switch (input)
-                    {
-                        case 1:
-                            Console.WriteLine("\nListing all games: ");
-                            var documents = gamesCollection.Find(new BsonDocument()).ToList();
-                            foreach (var document in documents)
-                                Console.WriteLine(document);
-                            break;
-                        case 2:
-                            Console.WriteLine("\nListing all Action games: ");
-                            var actiongamesFilter = Builders<BsonDocument>.Filter.Eq("genre", "Action");
-                            var actiongames = gamesCollection.Find(actiongamesFilter).ToList();
-                            foreach (var actiongame in actiongames)
-                                Console.WriteLine(actiongame);
-                            break;
-                        case 3:
-                            Console.WriteLine("\nListing all Roleplaying games: ");
-                            var rpggamesFilter = Builders<BsonDocument>.Filter.Eq("genre", "RPG");
-                            var rpggames = gamesCollection.Find(rpggamesFilter).ToList();
-                            foreach (var rpggame in rpggames)
-                                Console.WriteLine(rpggame);
-                            break;
-                        case 4:
-                            Console.WriteLine("\nListing all Strategy games: ");
-                            var stratgamesFilter = Builders<BsonDocument>.Filter.Eq("genre", "Strategy");
-                            var stratgames = gamesCollection.Find(stratgamesFilter).ToList();
-                            foreach (var stratgame in stratgames)
-                                Console.WriteLine(stratgame);
-                            break;
-                        case 5:
-                            Start();
-                            break;
-                        default:
-                            Console.WriteLine("Error. Try again.");
-                            break;
-                    }
-                }
-                else
-                {
-                    Console.WriteLine("Error. Try again.");
-                    ReadAll();
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex);
-                Start();
-            }
 
+            if (Int32.TryParse(Console.ReadLine(), out input))
+            {
+                switch (input)
+                {
+                    case 1:
+                        Console.WriteLine("\nListing all games: ");
+                        var documents = gamesCollection.Find(new BsonDocument()).ToList();
+                        foreach (var document in documents)
+                            Console.WriteLine(document);
+                        break;
+                    case 2:
+                        Console.WriteLine("\nListing all Action games: ");
+                        var actiongamesFilter = Builders<BsonDocument>.Filter.Eq("genre", "Action");
+                        var actiongames = gamesCollection.Find(actiongamesFilter).ToList();
+                        foreach (var actiongame in actiongames)
+                            Console.WriteLine(actiongame);
+                        break;
+                    case 3:
+                        Console.WriteLine("\nListing all Roleplaying games: ");
+                        var rpggamesFilter = Builders<BsonDocument>.Filter.Eq("genre", "RPG");
+                        var rpggames = gamesCollection.Find(rpggamesFilter).ToList();
+                        foreach (var rpggame in rpggames)
+                            Console.WriteLine(rpggame);
+                        break;
+                    case 4:
+                        Console.WriteLine("\nListing all Strategy games: ");
+                        var stratgamesFilter = Builders<BsonDocument>.Filter.Eq("genre", "Strategy");
+                        var stratgames = gamesCollection.Find(stratgamesFilter).ToList();
+                        foreach (var stratgame in stratgames)
+                            Console.WriteLine(stratgame);
+                        break;
+                    case 5:
+                        Start();
+                        break;
+                    default:
+                        Console.WriteLine("Error. Try again.");
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Error. Try again.");
+                ReadAll();
+            }
         }
 
         public void SearchGame()
@@ -131,7 +120,7 @@ namespace MongoTest
             var gamesCollection = database.GetCollection<BsonDocument>("Games");
 
             Console.WriteLine("= Search Game =");
-            Console.WriteLine("Enter Title:");
+            Console.WriteLine("Enter Game Title:");
             Console.Write("> ");
 
             string inp = Console.ReadLine();
@@ -140,14 +129,33 @@ namespace MongoTest
             Console.WriteLine("Results: ");
             try
             {
-                if (filter != null)
+
+                var doc = gamesCollection.Find(filter);
+                if (doc.Any())
                 {
-                    var doc = gamesCollection.Find(filter).FirstOrDefault();
-                    if (doc != null)
-                        Console.WriteLine(doc.ToString());
-                    else
-                        Console.WriteLine("Could not find game. Please try again.");
+                    foreach (var game in doc.ToList())
+                    {
+                        Console.WriteLine(game);
+                    }
                 }
+                else
+                {
+                    Console.WriteLine("Could not find game. Please try again.");
+                }
+
+                //if (filter != null)
+                //{
+                //    var doc = gamesCollection.Find(filter);
+                //    if (doc != null)
+                //        foreach (var game in doc)
+                //        {
+                //            Console.WriteLine(game);
+                //        }
+
+                //    //Console.WriteLine(doc.ToString());
+                //    else
+                //        Console.WriteLine("Could not find game. Please try again.");
+                //}
             }
             catch (Exception ex)
             {
@@ -164,53 +172,33 @@ namespace MongoTest
             {
                 string gameName = "";
                 string gameGenre = "";
-                int genreChoice = 0;
                 int gamePrice = 0;
                 int gameQuantity = 0;
 
                 Console.WriteLine("\n= Add Game =");
                 Console.Write("Game Title:\n>");
-
                 gameName = Console.ReadLine();
-                if (string.IsNullOrEmpty(gameName))
+
+                Console.Write("Genre:\n>");
+                gameGenre = Console.ReadLine();
+
+                Console.Write("Price:\n>");
+
+                if (Int32.TryParse(Console.ReadLine(), out gamePrice))
                 {
-                    Console.WriteLine("Invalid input. Please try again.");
-                    Start();
-                }
-                else
-                {
-                    Console.Write("Genre:\n>");
-
-
-                    if (Int32.TryParse(Console.ReadLine(), out genreChoice))
-
-
-                        //gameGenre = Console.ReadLine();
-
-                        if (string.IsNullOrEmpty(gameGenre))
-                        {
-                            Console.WriteLine("Invalid input. Please try again.");
-                            Start();
-                        }
-
-                    Console.Write("Price:\n>");
-
-                    if (Int32.TryParse(Console.ReadLine(), out gamePrice))
+                    Console.Write("Quantity:\n>");
+                    if (Int32.TryParse(Console.ReadLine(), out gameQuantity))
                     {
-                        Console.Write("Quantity:\n>");
-                        if (Int32.TryParse(Console.ReadLine(), out gameQuantity))
-                        {
-                            var document = new BsonDocument
+                        var document = new BsonDocument
             {
                 {"name", gameName},
                 {"genre", gameGenre},
                 {"price", gamePrice},
                 {"quantity", gameQuantity }
             };
-                            gamesCollection.InsertOne(document);
-                            Console.WriteLine($"The game \"{gameName}\" has been added.\n");
-                            Start();
-                        }
+                        gamesCollection.InsertOne(document);
+                        Console.WriteLine($"The game \"{gameName}\" has been added.\n");
+                        Start();
                     }
                     else
                     {
@@ -218,6 +206,12 @@ namespace MongoTest
                         CreateGame();
                     }
                 }
+                else
+                {
+                    Console.WriteLine("Error, could not add game.");
+                    CreateGame();
+                }
+
             }
             catch (Exception ex)
             {
@@ -231,7 +225,7 @@ namespace MongoTest
             var database = dbClient.GetDatabase("Product");
             var gamesCollection = database.GetCollection<BsonDocument>("Games");
 
-            Console.WriteLine("= Edit game =");
+            Console.WriteLine("= Edit Game =");
             Console.Write("Enter Game ID:\n>");
 
             try
@@ -247,7 +241,7 @@ namespace MongoTest
                 }
                 else
                 {
-                    Console.WriteLine($"Updating game \n{doc["name"].AsString}\n.");
+                    Console.WriteLine($"Updating game {doc["name"].AsString}.");
                     Console.WriteLine("What do you want to change?");
                     Console.Write("1. Title\n2. Genre\n3. Price\n4. Quantity\n>");
 
@@ -257,19 +251,15 @@ namespace MongoTest
                         {
                             case 1:
                                 string oldName = doc["name"].ToString();
-                                string newName = Console.ReadLine();
                                 Console.WriteLine($"Current title: {oldName}");
                                 Console.Write("Choose new title: \n>");
+                                string newName = Console.ReadLine();
 
                                 if (!string.IsNullOrWhiteSpace(newName))
                                 {
                                     var update = Builders<BsonDocument>.Update.Set("name", newName);
                                     gamesCollection.FindOneAndUpdate(filter, update);
                                     Console.WriteLine($"Game title successfully changed from \"{oldName}\" to \"{newName}\"\n");
-                                }
-                                else if (newName == "")
-                                {
-                                    Console.WriteLine("Invalid input. Please try again.");
                                 }
                                 else
                                 {
@@ -278,19 +268,15 @@ namespace MongoTest
                                 break;
                             case 2:
                                 string oldGenre = doc["genre"].ToString();
-                                string newGenre = Console.ReadLine();
                                 Console.WriteLine("Current genre: ");
                                 Console.Write("Choose new genre: \n>");
+                                string newGenre = Console.ReadLine();
 
                                 if (!string.IsNullOrWhiteSpace(newGenre))
                                 {
                                     var update = Builders<BsonDocument>.Update.Set("genre", newGenre);
                                     gamesCollection.FindOneAndUpdate(filter, update);
                                     Console.WriteLine($"Game genre successfully changed from \"{oldGenre}\" to \"{newGenre}\".\n");
-                                }
-                                else if (newGenre == "")
-                                {
-                                    Console.WriteLine("Invalid input. Please try again.");
                                 }
                                 else
                                 {
@@ -299,9 +285,9 @@ namespace MongoTest
                                 break;
                             case 3:
                                 string oldPrice = doc["price"].ToString();
-                                int newPrice = Int32.Parse(Console.ReadLine());
                                 Console.WriteLine($"Current price: {oldPrice}");
                                 Console.Write("Choose new price: \n>");
+                                int newPrice = Int32.Parse(Console.ReadLine());
 
                                 if (newPrice > 0)
                                 {
@@ -316,9 +302,9 @@ namespace MongoTest
                                 break;
                             case 4:
                                 string oldQuantity = doc["quantity"].ToString();
-                                int newQuantity = Int32.Parse(Console.ReadLine());
                                 Console.WriteLine($"Current quantity: {oldQuantity}");
                                 Console.Write("Choose new quantity: \n>");
+                                int newQuantity = Int32.Parse(Console.ReadLine());
 
                                 if (newQuantity >= 0)
                                 {
@@ -363,7 +349,7 @@ namespace MongoTest
                 var DeleteID = Builders<BsonDocument>.Filter.Eq("_id", new ObjectId(ID));
                 gamesCollection.DeleteOne(DeleteID);
                 var doc = gamesCollection.Find(DeleteID).FirstOrDefault();
-                Console.WriteLine($"Deleted {doc["name"].AsString} from database.");
+                Console.WriteLine("Deleted game from database.");
 
             }
             catch (Exception ex)
